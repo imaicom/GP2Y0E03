9f5daebd3d3c530d7b267ad6e68f349d04ec9ca6
KURURU <i@okako.com>
2019-08-13 00:27:55 +0900

    1
    1

--- gp2y0e03.c
+++ gp2y0e03.c
@@ -4,12 +4,12 @@
 #include <wiringPiI2C.h>
 #include <stdio.h>
  
-int main(){
+int main() {
 	
   int fd = wiringPiI2CSetup(0x40);
   int dat;
   
-  while(1){
+  while(1) {
     dat = wiringPiI2CReadReg8(fd, 0x5E) << 4 | wiringPiI2CReadReg8(fd, 0x5F);
     printf("%d(%x)\n", dat, dat);
     delay(1000);  // wait for 1sec
